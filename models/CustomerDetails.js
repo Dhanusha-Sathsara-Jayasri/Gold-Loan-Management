e7d14814e35ff7cb3e36947c1c93f5e5b8ba5945
dhanusha-sathsara-06 <dhanushasathsara06@gmail.com>
2024-08-16 16:17:04 +0530

    Updated

---
+++ models/CustomerDetails.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const customerDetailsSchema = new mongoose.Schema(
+  {
+  name: {
+    type: String,
+    required: [true, "Please Enter User Name"],
+  },
+  whatsApp: {
+    type: String,
+    required: [true, "Please Enter User WhatsApp Number"],
+  },
+  NIC: {
+    type: String,
+    unique: true,
+    required: [true, "Please Enter NIC"],
+  }
+}
+
+);
+
+const customerInfornations  = mongoose.model('Customer-Infomations', customerDetailsSchema);
+
+module.exports = customerInfornations;
