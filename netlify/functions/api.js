bab1cdd5e346345bf079c2ee5fc043c73f15d906
Dhanusha Sathsara <dhanushasathsara06@gmail.com>
2024-09-06 15:24:18 +0530

    Update MongoDB

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -3,29 +3,17 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const serverless = require('serverless-http');
 const mongoose = require('mongoose');
-const cors = require('cors');
 
 const router = express.Router();
 const app = express();
-
-// Enable CORS for all routes
-app.use(cors());
-
-// Middleware for parsing JSON requests
 app.use(bodyParser.json());
 
-
 // Database connection starts here
+router.use(express.json());
 mongoose
-  .connect('mongodb+srv://admin:admin@cluster0.gzqwq.mongodb.net/Gold-Loan-Management?retryWrites=true&w=majority&appName=Cluster0', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  })
+  .connect('mongodb+srv://intelmdb3gold:P6lJGy1cv64bl7Tf@gold-loans.bs6xu.mongodb.net/Gold-Loan-Management?retryWrites=true&w=majority&appName=Gold-Loans')
   .then(() => {
     console.log("Database Connected Successfully");
-  })
-  .catch((error) => {
-    console.error("Database Connection Failed", error);
   });
 
 
@@ -33,7 +21,16 @@ mongoose
 const customers = require('./models/CustomerDetails.js');
 const MarketValue = require('./models/addMarketValues.js');
 
-// Routers 
+// Requests send here
+router.post('/test3', async (req, res) => {
+  const test = {
+    name: "kk test3 done !",
+  };
+
+  res.send(test);
+});
+
+
 router.post('/addMarketValues', async (req, res) => {
   try {
     const { carats, date } = req.body;
@@ -81,27 +78,18 @@ router.post('/register', async (req, res) => {
     res.send({ status: "Error While Registering Customer", data: error });
   }
 
+  router.post('/test3', async (req, res) => {
+    const test = {
+      name: "kk test3 done !",
+    };
+  
+    res.send(test);
+  });
 });
 
 router.get('/test', async (req, res) => {
   const test = {
     name: "kk done !",
-  };
-
-  res.send(test);
-});
-
-router.get('/test3', async (req, res) => {
-  const test = {
-    name: "kk test3 get done !",
-  };
-
-  res.send(test);
-});
-
-router.post('/test3', async (req, res) => {
-  const test = {
-    name: "kk test3 post done !",
   };
 
   res.send(test);
