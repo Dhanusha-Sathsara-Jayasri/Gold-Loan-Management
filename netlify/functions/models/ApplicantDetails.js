016a2ca8ec899480a3c625a5378586ef73dc6d54
Dhanusha Sathsara <dhanushasathsara06@gmail.com>
2024-09-07 16:53:51 +0530

    add ApplicantDetails.js

---
+++ netlify/functions/models/ApplicantDetails.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+
+const applicantDetailsSchema = new mongoose.Schema({
+  phoneNumber: {
+    type: String,
+    required: true,
+    minlength: 10,
+    maxlength: 10
+  },
+  addressLine1: {
+    type: String,
+    required: true
+  },
+  addressLine2: {
+    type: String, // Optional field
+    default: ''
+  },
+  divisionalSecretariatDivision: {
+    type: String,
+    required: true
+  },
+  district: {
+    type: String,
+    required: true
+  },
+  gender: {
+    type: String,
+    enum: ['Male', 'Female'],
+    required: true
+  },
+  maritalStatus: {
+    type: String,
+    enum: ['Married', 'Unmarried'],
+    required: true
+  }
+});
+
+const ApplicantDetails = mongoose.model('Applicant-Details', applicantDetailsSchema);
+
+module.exports = ApplicantDetails;
