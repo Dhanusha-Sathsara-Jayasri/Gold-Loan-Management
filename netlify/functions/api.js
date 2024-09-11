1da8b6ad2c04d21023a1194f0b5624e901834e66
Dhanusha Sathsara Jayasri <dhanushasathsarajayasri@gmail.com>
2024-09-11 15:45:57 +0530

    Update api.js

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -47,7 +47,7 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage });
 
-app.post('/upload', upload.single('file'), (req, res) => {
+router.post('/upload', upload.single('file'), (req, res) => {
     if (!req.file) {
         return res.status(400).json({ message: 'No file uploaded' });
     }
