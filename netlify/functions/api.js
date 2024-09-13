ed51dbb290ed09e384882cc1465181900fdc5f3c
Dhanusha Sathsara Jayasri <dhanushasathsarajayasri@gmail.com>
2024-09-13 13:57:19 +0530

    Updated 3

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -5,7 +5,6 @@ const bodyParser = require('./middleware/bodyParser');
 
 const app = express();
 
-// Use Middleware
 app.use(cors);
 app.use(bodyParser);
 
@@ -29,15 +28,12 @@ const applicantRoutes = require('./routes/applicantRoutes');
 const mortgageDeedRoutes = require('./routes/mortgageDeedRoutes');
 const marketValueRoutes = require('./routes/marketValueRoutes');
 
-// Server starts here
+// Define and use routes
 const router = express.Router();
-
-// Define routes
 router.use('/register', customerRoutes);
 router.use('/application', applicantRoutes);
 router.use('/mortgageDeed', mortgageDeedRoutes);
 router.use('/api/', marketValueRoutes);
-
 
 router.get('/test', (req, res) => {
   res.send('Hello World');
--- netlify/functions/routes/mortgageDeedRoutes.js
+++ netlify/functions/routes/mortgageDeedRoutes.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-const MortgageDeedController = require('../controllers/mortgageDeedController');
-router.post('/addMortgageDeed', MortgageDeedController.addMortgageDeed);
+const mortgageDeedController = require('../controllers/mortgageDeedController');
+
+// Route to upload mortgage deed details
+router.post('/addMortgageDeed', mortgageDeedController.addMortgageDeed);
+
+// Route to get the uploaded image by filename
+router.get('/image/:filename', mortgageDeedController.getImage);
 
 module.exports = router;
