7ef1da142056cc5596cfd4ddc8a00227fdddfa65
Dhanusha Sathsara Jayasri <dhanushasathsarajayasri@gmail.com>
2024-09-19 16:34:54 +0530

    Updated

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,20 +1,21 @@
 const express = require('express');
 const serverless = require('serverless-http');
 const cors = require('./middleware/cors');
-const bodyParser = require('body-parser');
-const Grid = require('gridfs-stream');
-const mongoose = require('mongoose');
+// const bodyParser = require('./middleware/bodyParser');
+const bodyParser = require('body-parser'); 
 
 const app = express();
 
 // Use Middleware
-app.use(cors());  // Call cors as a function
+app.use(cors);
+// app.use(bodyParser);
 app.use(bodyParser.json({ limit: '10mb' }));
 app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
 
 // Database connection starts here
-//const url = 'mongodb+srv://admin:admin@cluster0.gzqwq.mongodb.net/Gold-Loan-Management?retryWrites=true&w=majority&appName=Cluster0'
-const url = 'mongodb+srv://intelmdb3gold:P6lJGy1cv64bl7Tf@gold-loans.bs6xu.mongodb.net/Gold-Loan-Management?retryWrites=true&w=majority&appName=Gold-Loans'
+const mongoose = require('mongoose');
+const url = 'mongodb+srv://admin:admin@cluster0.gzqwq.mongodb.net/Gold-Loan-Management?retryWrites=true&w=majority&appName=Cluster0'
+//const url = 'mongodb+srv://intelmdb3gold:P6lJGy1cv64bl7Tf@gold-loans.bs6xu.mongodb.net/Gold-Loan-Management?retryWrites=true&w=majority&appName=Gold-Loans'
 
 mongoose
   .connect(url, {
@@ -28,42 +29,21 @@ mongoose
     console.error("Database Connection Failed", error);
   });
 
-let gfs;
-const conn = mongoose.connection;
-
-conn.once('open', function () {
-  gfs = Grid(conn.db, mongoose.mongo);
-  gfs.collection('photos'); // Set GridFS collection name
-});
-
-// Routes for media retrieval
-app.get('/file/:filename', async (req, res) => {
-  try {
-    const file = await gfs.files.findOne({ filename: req.params.filename });
-    if (!file) return res.status(404).send('File not found');
-    
-    const readStream = gfs.createReadStream(file.filename);
-    readStream.pipe(res);
-  } catch (error) {
-    console.error("Error retrieving file:", error);
-    res.status(500).send('Server error');
-  }
-});
-
-// Routes for other functionalities
+// Routes
 const customerRoutes = require('./routes/customerRoutes');
 const applicantRoutes = require('./routes/applicantRoutes');
 const mortgageDeedRoutes = require('./routes/mortgageDeedRoutes');
 const marketValueRoutes = require('./routes/marketValueRoutes');
-const uploadImgRoutes = require('./routes/postRoutes');
+const postRoutes = require('./routes/postRoutes'); 
 
+// Server starts here
 const router = express.Router();
 
 // Define routes
 router.use('/register', customerRoutes);
 router.use('/application', applicantRoutes);
 router.use('/mortgageDeed', mortgageDeedRoutes);
-router.use('/file', uploadImgRoutes); 
+router.use('/posts', postRoutes); 
 router.use('/api/', marketValueRoutes);
 
 router.get('/test', (req, res) => {
