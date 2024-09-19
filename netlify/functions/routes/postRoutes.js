55ec8ea84acb8f62a9dc54b4b39f4f629c8702e1
Dhanusha Sathsara Jayasri <dhanushasathsarajayasri@gmail.com>
2024-09-19 16:15:49 +0530

    Update postRoutes.js

--- netlify/functions/routes/postRoutes.js
+++ netlify/functions/routes/postRoutes.js
@@ -1,7 +1,20 @@
+// const express = require('express');
+// const router = express.Router();
+
+// const postController = require('../controllers/postController');
+// router.post('/upload', postController.uploadImage);
+
+// module.exports = router;
+
+const upload = require('../controllers/postController');
 const express = require('express');
 const router = express.Router();
 
-const postController = require('../controllers/postController');
-router.post('/upload', postController.uploadImage);
+router.post("/upload",upload.single("file"),(req,res) => {
+
+    if(req.file === undefined) return res.send("you must select a file.");
+    const imgURL = `http://localhost:8080/file${req.file.filename}`;
+    return res.send(imgURL);
+});
 
 module.exports = router;
