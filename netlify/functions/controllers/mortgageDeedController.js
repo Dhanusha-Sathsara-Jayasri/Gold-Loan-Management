eaa82ed038d33e7b07583d867768399e149e0329
Dhanusha Sathsara <dhanushasathsarajayasri@gmail.com>
2024-09-29 14:45:27 +0530

    fix 11

--- netlify/functions/controllers/mortgageDeedController.js
+++ netlify/functions/controllers/mortgageDeedController.js
@@ -29,6 +29,27 @@ const mortgageDeedController = {
         } catch (error) {
             res.status(500).json({ status: "fail", message: "Error While Adding Mortgage Deed", data: error });
         }
+    },
+
+    getMortgageDeed: async (req, res) => {
+        const { customerId/* , customerApplicantDetailsId  */} = req.body;
+        if (!mongoose.Types.ObjectId.isValid(customerId)) {
+            return res.status(400).json({ status: "fail", message: "Invalid Customer ID" });
+        }
+       /*  if (!mongoose.Types.ObjectId.isValid(customerApplicantDetailsId)) {
+            return res.status(400).json({ status: "fail", message: "Invalid Applicant Details ID" });
+        } */
+        try {
+            const query = { customerId/* , customerApplicantDetailsId  */};
+            const mortgageDeed = await mortgageDeedModel.findOne(query);
+            if (mortgageDeed) {
+                res.status(200).json({ status: 'success', data: mortgageDeed });
+            } else {
+                res.status(200).json({ status: 'fail', data: "Mortgage Deed Not Found" });
+            }
+        } catch (error) {
+            res.status(500).json({ status: "fail", message: "Error While Fetching Mortgage Deed", data: error });
+        }
     }
 };
 
--- netlify/functions/routes/mortgageDeedRoutes.js
+++ netlify/functions/routes/mortgageDeedRoutes.js
@@ -3,5 +3,6 @@ const router = express.Router();
 
 const MortgageDeedController = require('../controllers/mortgageDeedController');
 router.post('/addMortgageDeed', MortgageDeedController.addMortgageDeed);
+router,post('/getMortgageDeed', MortgageDeedController.getMortgageDeed);
 
 module.exports = router;
