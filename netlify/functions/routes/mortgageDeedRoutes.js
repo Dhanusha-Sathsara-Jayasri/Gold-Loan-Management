87b93a8e75d2e8288f91a2b23c0163f2e9c8c1d1
Dhanusha Sathsara <dhanushasathsarajayasri@gmail.com>
2024-09-29 14:47:17 +0530

    fix 12

--- netlify/functions/routes/mortgageDeedRoutes.js
+++ netlify/functions/routes/mortgageDeedRoutes.js
@@ -3,6 +3,4 @@ const router = express.Router();
 
 const MortgageDeedController = require('../controllers/mortgageDeedController');
 router.post('/addMortgageDeed', MortgageDeedController.addMortgageDeed);
-router,post('/getMortgageDeed', MortgageDeedController.getMortgageDeed);
-
 module.exports = router;
